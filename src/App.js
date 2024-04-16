4ce4ec6dc34e0840b975fe612cf2df61015d54be
Abdul-Raqeeb13 <abdulraqeeb97568@gmail.com>
2024-04-16 17:58:44 +0500

    CallingAPI

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import ShowCount from "./Components/TestingUseEffect.jsx";
+
+import "./Components/StoreAPI_usingUseEffect.css"
+import StoreAPIcalling from "./Components/StoreAPI_usingUseEffect";
+
+function App() {
+  return (
+    // learninf useefect File
+    // <ShowCount/>
+
+    <StoreAPIcalling/>
+   
+  );
+}
+
+export default App;
